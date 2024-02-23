49fb3274d8fd8b625157a3e2ac00a7ac39953bdb
GaiZheWong <gzw1c20@soton.ac.uk>
2024-02-23 16:00:29 +0000

    Fixed Linear Actuator Test

--- test/LinearActuatorTest.py
+++ test/LinearActuatorTest.py
@@ -1,6 +1,7 @@
 # Standard Imports
 
 # Third Party Imports
+import RPi.GPIO as GPIO
 
 # Project Specific Imports
 from HardwareComponents.LinearActuator import LinearActuator
@@ -11,8 +12,8 @@ Motor1In1 = 29
 Motor1In2 = 31
 Motor1EN  = 32   # PWM Pin on Raspberry PI
 
+GPIO.setmode(GPIO.BOARD)
 LinearMotor = LinearActuator(In1=Motor1In1, In2=Motor1In2, EN=Motor1EN)
-
 
 while True:
 
