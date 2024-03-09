920ffcd93f52f798308c605987f45147d73fbf0a
Ken Ooi <kenooimh@gmail.com>
2024-03-09 00:05:54 +0000

    Added config

--- test/StepperMotorTest.py
+++ test/StepperMotorTest.py
@@ -3,13 +3,28 @@ import time
 import RPi.GPIO as GPIO
 
 from HardwareComponents.StepperMotor import StepperMotor
+"""
+Cable config: black to black
+green to yellow
+red to red
+blue to blue
 
+Motor driver position, heat sink on your left
+Bottom. Left to right: Yellow blue
+Top. Left to right: Red black
+"""
 
 if __name__ == '__main__':
 
 
     motor = StepperMotor()
+    
+    #applies to current config. Don't change sleep_time.
 
-    motor.step_anticlockwise(steps=1000, sleep_time=0.001)
+    #retracts platform
+    motor.step_anticlockwise(steps=10, sleep_time=0.001)
+    
+    #extends platform
+    #motor.step_clockwise(steps=10, sleep_time=0.001)
 
     GPIO.cleanup()
