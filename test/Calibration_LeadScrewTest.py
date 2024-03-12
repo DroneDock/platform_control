d2662b2b467b59a2e91046d53d82211b853ef859
Ken Ooi <kenooimh@gmail.com>
2024-03-12 11:41:54 +0000

    Simple manual w/s calibration method

---
+++ test/Calibration_LeadScrewTest.py
@@ -0,0 +1,45 @@
+import time
+
+import RPi.GPIO as GPIO
+
+from HardwareComponents.StepperMotor import StepperMotor
+"""
+This is for lead screw
+Cable config: black to black
+green to yellow
+red to red
+blue to blue
+
+Motor driver position, heat sink on your left
+Bottom. Left to right: Yellow blue
+Top. Left to right: Red black
+
+Anticlockwise is retract
+Clockwise is extend
+
+Works once in a while but overheating issues as well.
+"""
+#classify steps
+step = 50
+
+if __name__ == "__main__":
+
+
+    motor = StepperMotor()
+    
+    while True: 
+
+        key = input('Press W to increase rotate clockwise , press S to rotate anticlockwise: ')
+
+        if key == 'w':
+            motor.step_clockwise(steps=step, sleep_time=0.001)
+        elif key == 's':
+            motor.step_anticlockwise(steps=step, sleep_time=0.001)
+    
+        elif key == 'b':
+            break
+   
+      
+  
+    GPIO.cleanup()
+  
