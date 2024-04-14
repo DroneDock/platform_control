2d4c8a93ea4cdf420e3ac0fab8a8529ab9c8d86d
JalenTan1505 <jalen.tk@gmail.com>
2024-04-14 17:19:21 +0100

    feat/upgraded dcMotor class
    Added duration argument to methods to control duration of rotation. Also updated DCMotorTest to utilize this class

--- HardwareComponents/DCMotor.py
+++ HardwareComponents/DCMotor.py
@@ -1,3 +1,5 @@
+# Standard Imports
+import time
 # Third Party Imports
 import RPi.GPIO as GPIO
 
@@ -5,17 +7,16 @@ import RPi.GPIO as GPIO
 class  DCMotor(object):
     """
     DC motor class to control the motor.
-
-    Attributes
-    ----------
-
-
-    Methods
-    -------
-
+    
+    This class is developed based on the DC motor directly controlling arm
+    angle. The methods "forward" and "backward" assumes the following cable
+    config:
+    
+    With heat sink positioned at the left side, place the black wire to the 
+    left of the red wire.
     """
 
-    def __init__(self, In1: int, In2: int, EN:int, input_mode=GPIO.BCM):
+    def __init__(self, In1: int, In2: int, EN:int):
         """Initialize a DC Motor class used to control the motor.
 
         Args:
@@ -40,44 +41,56 @@ class  DCMotor(object):
 
         # Configure and start PWM
         self._PWM = GPIO.PWM(self._PWMPin, 2000)  # Set PWM frequency as 2000
-        self._PWM.start(50)   # Start the PWM instance at zero duty cycle (i.e. static)
+        self._PWM.start(0)   # Start the PWM instance at zero duty cycle (i.e. static)
 
-
-    def clockwise(self, dutyCycle:int = 100) -> None:
-        """(Public) Rotate the DC motor clockwise at a given duty cycle.
+    def forward(self, dutyCycle:int = 100, duration:int = -1) -> None:
+        """(Public) Rotate the DC motor clockwise at a given duty cycle for a
+        specified duration.
 
         Args:
-            DutyCycle (int, optional): PWM duty cycle supplied to the motor. 
+            dutyCycle (int, optional): PWM duty cycle supplied to the motor. 
             Defaults to 100.
+            duration (int, optional): Duration in seconds for which the motor
+            will rotate. Default is -1, which causes continuous rotation.
         """
-        
         GPIO.output(self._in1Pin, GPIO.LOW)
         GPIO.output(self._in2Pin, GPIO.HIGH)
         self._PWM.ChangeDutyCycle(dutyCycle)
+        
+        if duration >= 0:
+            time.sleep(duration)
+            self._PWM.ChangeDutyCycle(0)
+            self.stop()
 
-
-    def anticlockwise(self, dutyCycle:int = 100) -> None:
-        """(Public) Rotate the DC Motor anticlockwise at a given duty cycle,
+    def backward(self, dutyCycle:int = 100, duration: int = -1) -> None:
+        """(Public) Rotate the DC Motor anticlockwise at a given duty cycle for
+        a specified duration.
 
         Args:
             DutyCycle (int, optional): PWM duty cycle supplied to the motor.
             Defaults to 100.
+            duration (int, otption): Duration in seconds for which the motor
+            will rotate. Default is -1, which causes continuous rotation.
         """
-
         GPIO.output(self._in1Pin, GPIO.HIGH)
         GPIO.output(self._in2Pin, GPIO.LOW)
         self._PWM.ChangeDutyCycle(dutyCycle)
+        
+        if duration >= 0:
+            time.sleep(duration)
+            self._PWM.ChangeDutyCycle(0)
+            self.stop()
 
     def stop(self) -> None:
         """
         (Public) Stop the DC motor from rotating.
         
-        Implementation: set the duty cycle to zero.
+        Implementation: set the duty cycle to zero. (might be unsafe to leave PWM channel open?)
         """
         self._PWM.ChangeDutyCycle(0)
 
-
     def __del__(self):
         """Destructor to cleanup GPIO ports"""
+        self._PWM.stop()
         GPIO.cleanup()
     
--- test/DCMotorTest.py
+++ test/DCMotorTest.py
@@ -1,51 +1,26 @@
 """
-Author: Ken
+Author: Gai Zhe
 
 Description:
+- Revamped DC Motor Test which utilizes the DC Motor class
 - Cable config for L298N motor driver: With heat sink positioned at the left 
 side, place the black wire to the left of the red wire.
 
 How to use:
 W to go forward for 1 second
 S to go backward for 1 second
-
-Progress:
-This is working with the new soldering (April 14)
 """
 
 # Standard Imports
 import time
-
-# Third Party Imports
+# Third-Party Imports
 import RPi.GPIO as GPIO
+# Project-Specific Imports
+from HardwareComponents.DCMotor import DCMotor
 
-
-# Pin Definition using BCM numbering system -----------------------------------
-Motor1In1 = 17
-Motor1In2 = 27
-Motor1EN  = 18   # PWM Pin on Raspberry PI
+# Instantiate DC Motor object
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(Motor1In1, GPIO.OUT)
-GPIO.setup(Motor1In2, GPIO.OUT)
-GPIO.setup(Motor1EN, GPIO.OUT)
-
-# Specify direction
-#GPIO.output(Motor1In1, GPIO.HIGH)
-#GPIO.output(Motor1In2, GPIO.LOW)
-
-# Setup PWM
-p = GPIO.PWM(Motor1EN, 2000)  # Set PWM frequency (in Hz)
-
-# Start PWM signals, specifying the duty cycle (in %)
-# Specify the DutyCycle (speed), max rate at 100
-DutyCycle = 100
-
-"""
-Press W to go clockwise (down)
-Press S to go anticlockwise (up)
-Press b to stop
-"""
+dcMotor = DCMotor(In1=17, In2=27, EN=18)
 
 while True:
 
@@ -53,31 +28,19 @@ while True:
         key = input('Press W to go clockwise, press S to go anticlockwise: ')
 
         if key == 'w':  # Goes forward
-            GPIO.output(Motor1In1, GPIO.LOW)
-            GPIO.output(Motor1In2, GPIO.HIGH)
-            print("Going clockwise")
-            p.start(DutyCycle)
-            time.sleep(1)
-            p.stop()
+            dcMotor.forward(duration=1)
 
         elif key == 's': # Goes backwards
-            GPIO.output(Motor1In1, GPIO.HIGH)
-            GPIO.output(Motor1In2, GPIO.LOW)
-            print("Going anticlockwise")
-            p.start(DutyCycle)
-            time.sleep(1)
-            p.stop()
+            dcMotor.backward(duration=1)
 
         elif key == 'b':
-            p.stop()
+            dcMotor.stop()
             break
 
    # p.ChangeDutyCycle(DutyCycle)
    # print(f"The duty cycle is {DutyCycle}")
     except KeyboardInterrupt:
-
-        # Stop PWM
-        p.stop()
+        dcMotor.stop()
         GPIO.cleanup()
 
 GPIO.cleanup()
